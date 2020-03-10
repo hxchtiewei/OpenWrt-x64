1bf6d264a2652e81fadddec81dbf13002c62869d
hxchtiewei <48231900+hxchtiewei@users.noreply.github.com>
2020-03-10 10:04:20 +0800

    1
    1

--- diy.sh
+++ diy.sh
@@ -5,5 +5,13 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 定制默认IP
+#sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
+
+# 替换默认Argon主题
+# rm -rf package/lean/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
+
+# 添加第三方软件包
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
