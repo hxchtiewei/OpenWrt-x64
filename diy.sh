b1e6de8485de5f336b04a0e4fd4fa524dc3d2982
hxchtiewei <48231900+hxchtiewei@users.noreply.github.com>
2020-03-12 23:33:22 +0800

    3

--- diy.sh
+++ diy.sh
@@ -6,11 +6,11 @@
 # Blog: https://p3terx.com
 #=================================================
 # 定制默认IP
-#sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
 
 # 替换默认Argon主题
-rm -rf package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+# rm -rf package/lean/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 # git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
 
 # 添加第三方软件包
